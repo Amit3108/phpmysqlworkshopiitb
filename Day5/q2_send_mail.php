89333ebede8e96ae896d566717ac8f4b9917f9b8
unknown <naikamje19ie@student.mes.ac.in>
2020-10-13 14:59:14 +0530

    fifth commit

--- Day5/q2_send_mail.php
+++ Day5/q2_send_mail.php
@@ -1,41 +1,46 @@
-<html>
-<head><b>Feedback</b></head>
-<body>
-<p>Please fill the feedback form as your feedback matters to us.</p>
-<form action = "q2_send_mail.php" method="POST">
-    Name <input type="text"  name="name" style="margin-left:25px;" required><br><br>
-    Mail Id<input type="email"  name="mail" style="margin-left:19px;" required><br><br>
-    <label for="feedback">Feedback</label>
-    <textarea id="feedback" name="feedback" rows="4" cols="50"></textarea><br><br>
-    <input type="submit" name="submit">
- </form>
-</body>
-</html>
-
 <?php
 
-if(@$_POST["submit"])
-{
- echo "Thanks for giving feedback.";   
- $name = @$_POST["name"];
- $mailto = @$_POST["mail"];
- $adminmail= "example@gmail.com";
- $feedback = @$_POST["feedback"];
- $subject = "Feedback";
- $msg = "Hey $name thanks for filling feedback. Here is what we got from you: \n";
- $finalmsg = $msg . $feedback;
- $finalmsg = wordwrap($finalmsg, 70);
- $headers = "From: example@gmail.com";
- $headers1 = "From: $mailto";
+//'Send me an email' script
 
- $msg1 = "Here is what we got from $name ($mailto): \n";
- $finalmsg1 = $msg1 . $feedback;
- $finalmsg1 = wordwrap($finalmsg1,70); 
- $headers1 = "From: $mailto";
- 
- mail($mailto,$subject,$finalmsg,$headers);
- mail($adminmail,$subject,$finalmsg1,$headers1);
+if($_POST['submit'])
+{
+//get data from form
+$name = $_POST['name'];
+$message = $_POST['message'];
+
+if($name && $message) // existence check
+	{
+		$namelen=20;
+		$messagelen=300;
+		if(strlen($name)<=$namelen && strlen($message)<=$messagelen) //length  check
+		{
+			ini_set("SMTP","smtp.gmail.com");
+			ini_set("smtp_port","587");
+			//setup variables
+			$to = "example@gmail.com";
+			$subject = "blah";
+			$headers="From: example@gmail.com";
+			$body ="This email from $name\n\n$message";
+
+			$blah = mail($to, $subject, $body, $headers);
+			echo $blah;
+
+		}
+		else
+		    die("Max length for name is $namelen, and max length for message $messagelen");
+ 	}
+else
+	die("You must enter a name <u>and</u> message");
 
 }
-
 ?>
+
+<html>
+
+<form action='q2_send_mail.php' method='POST'>
+	Name: <input type='text' name='name' maxlength='20'><br />
+	Message:<br /><textarea name='message'> </textarea><p>
+	<input type='submit' name='submit' value='send me this'>
+</form>
+
+</html>
